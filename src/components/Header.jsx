4ac3cd0d744b95c8650053be4b0ceef83121ddf9
Matheus Vinicius Engelesberger <engelesberger.matheus@hotmail.com>
2026-04-24 16:16:26 -0300

    alteracoes no css para melhor interface

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 const Header = () => {
-  return <h2>Anime Quotes</h2>;
+  return (
+    <header className="header">
+      <h1 className="header-title">⛩ Anime Quotes</h1>
+      <p className="header-sub">Frases icônicas do universo anime</p>
+    </header>
+  )
 };
 
 export default Header;
