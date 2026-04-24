9dab45dcd52dd146ef8d964e1a8b0f3efed95d98
Matheus Vinicius Engelesberger <engelesberger.matheus@hotmail.com>
2026-04-24 11:31:42 -0300

    limpar estrutura inicial e cria componentes base

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,10 @@
+const SearchForm = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Digite o nome do anime" />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+};
+
+export default SearchForm;
